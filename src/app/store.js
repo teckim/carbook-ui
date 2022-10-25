0c0b9cc7e059a8def93d070b1aeec4c1c98944af
ridwan <ridwanediallo99@gmail.com>
2022-10-25 12:38:35 +0000

    Add car details to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import carDetailsReducer from '../features/car/carDetailsSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    details: carDetailsReducer,
   },
 });
 
