df75a468c42b90719af3c80a81446c56ae3c113f
ridwan <ridwanediallo99@gmail.com>
2022-10-25 14:48:41 +0000

    Remove boilerplate

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import carDetailsReducer from '../features/car/carDetailsSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
     details: carDetailsReducer,
   },
 });
