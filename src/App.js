3164c0079c5cc378f4da4da617d83ad95455e840
BHDHakim <hakim.bhd@gmail.com>
2022-10-25 15:22:03 +0100

    Remove App.css from App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { authRouter } from './app/router';
 import AuthLayout from './features/auth/AuthLayout';
-import './App.css';
 
 function App() {
   return <AuthLayout><RouterProvider router={authRouter} /></AuthLayout>;
