b9d87862ea5d5aa55ab96d7948cbeb3babb94bea
BHDHakim <hakim.bhd@gmail.com>
2022-10-25 12:04:45 +0100

    Add auth to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import authReducer from '../features/auth/authSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    auth: authReducer,
   },
 });
 
