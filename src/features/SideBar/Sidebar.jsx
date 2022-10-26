429267342035a9a3c0acb90a49c1a4e16bdf215c
HanningtoneM <hanningtonepompyy@gmail.com>
2022-10-26 10:20:54 +0300

    Add navlink home

--- src/features/SideBar/Sidebar.jsx
+++ src/features/SideBar/Sidebar.jsx
@@ -8,7 +8,35 @@ const Sidebar = () => {
   };
 
   return (
-  
+    <>
+      <div
+        className={`${styles.sidebarCnt} container-fluid overflow-hidden d-none d-md-block position-fixed start-0 top-0`}
+      >
+        <div className="vh-100 overflow-auto w-100">
+          <div className={`${styles.navbarContainer} vh-100 pt-5`}>
+            <div className="d-flex flex-sm-column flex-row flex-grow-1 align-items-center align-items-sm-start">
+              <NavLink
+                to="/home"
+                className="d-flex align-items-center pb-sm-3 mb-3 me-md-auto text-decoration-none"
+              >
+                <span className={`${styles.brandName} fs-1 ps-4`}>
+                  CarBook
+                </span>
+              </NavLink>
+              <ul
+                className="nav nav-pills flex-sm-column flex-row flex-nowrap flex-shrink-1 flex-sm-grow-0 flex-grow-1 mb-sm-auto mb-0 justify-content-center align-items-center align-items-sm-start"
+                id="menu"
+              >
+                <li className="w-100">
+                  <NavLink to="/home" className="nav-link mb-2 fs-5">
+                    <i className="fa-solid fa-house me-2" />
+                    <span className="ms-1 d-none d-sm-inline">Home</span>
+                  </NavLink>
+                </li>
+                <li className="w-100">
+                  
+      </nav>
+    </>
   );
 };
 
