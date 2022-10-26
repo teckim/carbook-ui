0b0b42fe34983877915c93d3351ff58519eb3cdd
HanningtoneM <hanningtonepompyy@gmail.com>
2022-10-26 10:23:56 +0300

    Add button Logout

--- src/features/SideBar/Sidebar.jsx
+++ src/features/SideBar/Sidebar.jsx
@@ -63,4 +63,13 @@ const Sidebar = () => {
                   </NavLink>
                 </li>
 
+                <li className="w-100 nav-link mb-2 mt-2">
+                  <button
+                    type="button"
+                    className="btn btn-danger d-none d-sm-inline fs-5"
+                    onClick={() => logout()}
+                  >
+                    <i className="fa-solid fa-arrow-right-from-bracket me-2" />
+                    Logout
+                  </button>
                
