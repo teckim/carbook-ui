6ef69b9f31ce996d9c96b3939bdf2b79d25bf224
bigmosi <kinyeramo@gmail.com>
2022-10-26 23:15:32 +0300

    add my reservation reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import carDetailsReducer from '../features/car/carDetailsSlice';
 import myReservationsReducer from '../features/myReservations/myReservation';
+import reservationsReducer from '../features/Reservations/Reservations';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     details: carDetailsReducer,
-    myReservationsReducer,
+    myreseravation: myReservationsReducer,
+    reservations: reservationsReducer,
   },
 });
 
