a73f3f627f1ee22ea676c17874d97813f7ea251c
ridwan <ridwanediallo99@gmail.com>
2022-10-26 11:02:11 +0000

    Update carDetailsSlice

--- src/features/car/carDetailsSlice.js
+++ src/features/car/carDetailsSlice.js
@@ -1,24 +1,44 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import CarData from './carData';
 
-export const getCarDetails = createAsyncThunk(
-  'carDetails/getCarDetails',
-  async (id) => {
-    // const res = await fetch('http://localhost:3000/v1/cars/');
-    const res = await fetch(`http://localhost:3000/v1/cars/${id}`);
-    const data = await res.json();
-    return data;
-  },
-);
+export const retrieveCars = createAsyncThunk('cars/retrieve', async () => {
+  const res = await CarData.getAll();
+  console.log(res.data);
+  return res.data;
+});
+
+export const retrieveCar = createAsyncThunk('car/retrieve', async (id) => {
+  const res = await CarData.get(id);
+  console.log(res.data);
+  return res.data;
+});
+
+export const removeCar = createAsyncThunk('cars/delete', async ({ id }) => {
+  await CarData.remove(id);
+  return { id };
+});
 
 const carDetailsSlice = createSlice({
   name: 'carDetails',
   initialState: [],
 
+  addCar: (state, action) => {
+    state.push(action.payload);
+  },
+  deleteCar: () => {},
+
   extraReducers: {
-    [getCarDetails.fulfilled]: (state, action) => action.payload,
+    [retrieveCars.fulfilled]: (state, action) => [action.payload],
+
+    [retrieveCar.fulfilled]: (state, action) => action.payload,
+
+    [removeCar.fulfilled]: (state, action) => {
+      const index = state.findIndex(({ id }) => id === action.payload.id);
+      state.splice(index, 1);
+    },
   },
 });
 
-// export const { greeting } = carDetailsSlice.actions;
+export const { addCar, deleteCar } = carDetailsSlice.actions;
 
 export default carDetailsSlice.reducer;
