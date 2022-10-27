85e7331adfe7fd7f5ae976a3b461c35f2f45483c
bigmosi <kinyeramo@gmail.com>
2022-10-27 10:32:47 +0300

    added my reservations page

--- src/app/components/MyReservation.js
+++ src/app/components/MyReservation.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
-const MyReservation = () => <div><h1>My Reservation</h1></div>;
+const MyReservation = () => (
+  <div>
+    <h1>ALL RESERVATIONS</h1>
+    <div>
+      <div>
+        <div>
+          <div>
+            <p>Car:</p>
+            <p>Prado</p>
+          </div>
+          <div>
+            <p>City:</p>
+            <p>Kampala</p>
+          </div>
+          <div>
+            <p>Date:</p>
+            <p>2022-10-8</p>
+          </div>
+        </div>
+        <button type="button">
+          Delete
+        </button>
+      </div>
+    </div>
+  </div>
+);
 
 export default MyReservation;
