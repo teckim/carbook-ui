477445c8f1abe1ac1c86e04ad6b309538ad7ffa2
ridwan <ridwanediallo99@gmail.com>
2022-10-27 07:12:38 +0000

    Display car data

--- src/app/components/carDetails/CarDetails.js
+++ src/app/components/carDetails/CarDetails.js
@@ -4,13 +4,15 @@ import { useParams } from 'react-router-dom';
 import {
   retrieveCars,
   retrieveCar,
+  removeCar,
 } from '../../../features/car/carDetailsSlice';
+import './carDetails.css';
+import car from './car.jpg';
 
 const CarDetails = () => {
   const dispatch = useDispatch();
   const details = useSelector((state) => state.details);
   const dataFetchedRef = useRef(false);
-  console.log(details);
 
   const { id } = useParams();
   useEffect(() => {
@@ -22,9 +24,37 @@ const CarDetails = () => {
 
   return (
     <>
-      <div key={details.id}>
-        <h1>{details.brand}</h1>
-        <h1>{details.model}</h1>
+      {/* <div key={details.id} className="columns-1 md:columns-2 mx-8">
+        <img className="w-full" src={car} alt="car" />
+        <div className="details mx-8">
+          <h1 className="text-3xl font-bold underline">{details.brand}</h1>
+          <h1>{details.model}</h1>
+          <h1>{details.description}</h1>
+        </div>
+      </div> */}
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        {/* <figure> */}
+        <img src={car} alt="car" />
+        {/* </figure> */}
+        <div className="card-body">
+          <h2 className="card-title">{details.brand}</h2>
+          <p>{details.description}</p>
+          <p>
+            <strong>{details.price}</strong>
+          </p>
+          <div className="card-actions">
+            <button
+              type="button"
+              className="btn bg-secondary-focus"
+              onClick={() => dispatch(removeCar(details))}
+            >
+              Delete the car
+            </button>
+            <button type="button" className="btn btn-primary">
+              Reserve me
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
