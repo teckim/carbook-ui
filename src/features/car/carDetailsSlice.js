efc98d8091e6fe2ca655fc28bbb3115dc8994567
ridwan <ridwanediallo99@gmail.com>
2022-10-28 10:55:31 +0000

    Add createCar async thunk

--- src/features/car/carDetailsSlice.js
+++ src/features/car/carDetailsSlice.js
@@ -1,6 +1,22 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import CarData from './carData';
 
+export const createCar = createAsyncThunk(
+  'car/create',
+  async ({
+    brand, model, image, price, description,
+  }) => {
+    const res = await CarData.create({
+      brand,
+      model,
+      image,
+      price,
+      description,
+    });
+    return res.data;
+  },
+);
+
 export const retrieveCars = createAsyncThunk('cars/retrieve', async () => {
   const res = await CarData.getAll();
   console.log(res.data);
@@ -22,12 +38,10 @@ const carDetailsSlice = createSlice({
   name: 'carDetails',
   initialState: [],
 
-  addCar: (state, action) => {
-    state.push(action.payload);
-  },
-  deleteCar: () => {},
-
   extraReducers: {
+    [createCar.fulfilled]: (state, action) => {
+      state.push(action.payload);
+    },
     [retrieveCars.fulfilled]: (state, action) => [action.payload],
 
     [retrieveCar.fulfilled]: (state, action) => action.payload,
