f8ec447d2652705a75224dd98ab83e2094aaaf83
ridwan <ridwanediallo99@gmail.com>
2022-10-28 10:54:20 +0000

    Update returned values and remove car import.

--- src/app/components/carDetails/CarDetails.js
+++ src/app/components/carDetails/CarDetails.js
@@ -7,7 +7,6 @@ import {
   removeCar,
 } from '../../../features/car/carDetailsSlice';
 import './carDetails.css';
-import car from './car.jpg';
 
 const CarDetails = () => {
   const dispatch = useDispatch();
@@ -23,40 +22,55 @@ const CarDetails = () => {
   }, [dispatch]);
 
   return (
-    <>
-      {/* <div key={details.id} className="columns-1 md:columns-2 mx-8">
-        <img className="w-full" src={car} alt="car" />
-        <div className="details mx-8">
-          <h1 className="text-3xl font-bold underline">{details.brand}</h1>
-          <h1>{details.model}</h1>
+    <div className="flex items-center h-screen">
+      <div key={details.id} className="columns-1 md:columns-2 mx-8">
+        <img className="w-full" src={details.image} alt="car" />
+        <div className="mx-8">
+          <h1 className="text-3xl font-bold">{details.brand}</h1>
           <h1>{details.description}</h1>
-        </div>
-      </div> */}
-      <div className="card lg:card-side bg-base-100 shadow-xl">
-        {/* <figure> */}
-        <img src={car} alt="car" />
-        {/* </figure> */}
-        <div className="card-body">
-          <h2 className="card-title">{details.brand}</h2>
-          <p>{details.description}</p>
-          <p>
-            <strong>{details.price}</strong>
-          </p>
-          <div className="card-actions">
-            <button
-              type="button"
-              className="btn bg-secondary-focus"
-              onClick={() => dispatch(removeCar(details))}
-            >
-              Delete the car
-            </button>
-            <button type="button" className="btn btn-primary">
-              Reserve me
-            </button>
+          <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <tbody>
+                <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Model
+                  </th>
+                  <td className="py-4 px-6">{details.model}</td>
+                </tr>
+                <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    Price
+                  </th>
+                  <td className="py-4 px-6">{details.price}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
+        <div className="flex md:mt-64 justify-around">
+          <button
+            type="button"
+            className="btn bg-red-700"
+            onClick={() => dispatch(removeCar(details))}
+          >
+            Delete car
+          </button>
+          <button
+            type="button"
+            className="btn bg-green-700"
+            onClick={() => dispatch(removeCar(details))}
+          >
+            Reserve car
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default CarDetails;
