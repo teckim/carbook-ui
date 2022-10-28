b6b5963f9706a6d91a618d09b4893b022ecfc901
swaibat <sswaib11@gmail.com>
2022-10-29 01:13:59 +0300

    description of the application

--- src/app/components/Reserve.js
+++ src/app/components/Reserve.js
@@ -15,7 +15,7 @@ function Reserve() {
       <div className="container showcase-inner">
         <h1>Connect with vetted software developer</h1>
         <p className="about">
-          Get me a mentor app is built as a capstone project by a group of 5
+          Get me a Car app is built as a capstone project by a group of 4
           microverse talented software developers who want to make the
           engineering world a better place to collaborate. by providing a
           platform to connect experienced and upcoming engineering.
@@ -32,7 +32,7 @@ function Reserve() {
               <input type="" name="country" placeholder="country" />
               <input type="text" name="city" placeholder="city" />
 
-              <select name="mentor_id">
+              <select name="car_id">
                 <option>Kinyera</option>
               </select>
               <input className={style.date} type="date" name="date" id="" />
