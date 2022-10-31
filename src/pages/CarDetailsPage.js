3a56f6c6025edc3fd3810a3e381c74c98ff984e4
ridwan <ridwanediallo99@gmail.com>
2022-10-31 11:19:41 +0000

    Updat e car details style

--- src/pages/CarDetailsPage.js
+++ src/pages/CarDetailsPage.js
@@ -21,52 +21,56 @@ const CarDetailsPage = () => {
     dispatch(retrieveCar(id));
   }, [dispatch, id]);
   return (
-    <div className="flex items-center h-screen">
-      <div key={details.id} className="columns-1 md:columns-2 mx-8">
-        <img className="w-full" src={details.image} alt="car" />
-        <div className="mx-8">
-          <h1 className="text-3xl font-bold">{details.brand}</h1>
-          <h1>{details.description}</h1>
-          <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <tbody>
-                <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                  <th
-                    scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Model
-                  </th>
-                  <td className="py-4 px-6">{details.model}</td>
-                </tr>
-                <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
-                  <th
-                    scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Price
-                  </th>
-                  <td className="py-4 px-6">{details.price}</td>
-                </tr>
-              </tbody>
-            </table>
+    <div className="details">
+      <div className="flex items-center h-screen">
+        <div key={details.id} className="columns-1 md:columns-2 mx-8">
+          <img className="w-full" src={details.image} alt="car" />
+          <div className="mx-8 flex flex-col">
+            <div className="justify-self-end">
+              <h1 className="text-3xl font-bold">{details.brand}</h1>
+              <h1>{details.description}</h1>
+            </div>
+            <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <tbody>
+                  <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Model
+                    </th>
+                    <td className="py-4 px-6">{details.model}</td>
+                  </tr>
+                  <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Price
+                    </th>
+                    <td className="py-4 px-6">{details.price}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="flex md:mt-24">
+                {/* <button
+                  type="button"
+                  className="btn bg-red-700 rounded-full"
+                  onClick={() => dispatch(removeCar(details))}
+                >
+                  Delete car
+                </button> */}
+                <button
+                  type="button"
+                  className="btn btn-style justify-self-end rounded-full"
+                  onClick={() => dispatch(removeCar(details))}
+                >
+                  Reserve car
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="flex md:mt-64 justify-around">
-          <button
-            type="button"
-            className="btn bg-red-700"
-            onClick={() => dispatch(removeCar(details))}
-          >
-            Delete car
-          </button>
-          <button
-            type="button"
-            className="btn bg-green-700"
-            onClick={() => dispatch(removeCar(details))}
-          >
-            Reserve car
-          </button>
         </div>
       </div>
     </div>
