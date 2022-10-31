443d0d935ada2df529e34e6c0bb1f6573a6e73d6
ridwan <ridwanediallo99@gmail.com>
2022-10-31 11:18:08 +0000

    Remove justify around

--- src/app/components/carDetails/CarDetails.js
+++ src/app/components/carDetails/CarDetails.js
@@ -22,55 +22,56 @@ const CarDetails = () => {
   }, [dispatch]);
 
   return (
-    <div className="flex items-center h-screen">
-      <div key={details.id} className="columns-1 md:columns-2 mx-8">
-        <img className="w-full" src={details.image} alt="car" />
-        <div className="mx-8">
-          <h1 className="text-3xl font-bold">{details.brand}</h1>
-          <h1>{details.description}</h1>
-          <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <tbody>
-                <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                  <th
-                    scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Model
-                  </th>
-                  <td className="py-4 px-6">{details.model}</td>
-                </tr>
-                <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
-                  <th
-                    scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Price
-                  </th>
-                  <td className="py-4 px-6">{details.price}</td>
-                </tr>
-              </tbody>
-            </table>
+
+      <div className="flex items-center h-screen">
+        <div key={details.id} className="columns-1 md:columns-2 mx-8">
+          <img className="w-full" src={details.image} alt="car" />
+          <div className="mx-8">
+            <h1 className="text-3xl font-bold">{details.brand}</h1>
+            <h1>{details.description}</h1>
+            <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <tbody>
+                  <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Model
+                    </th>
+                    <td className="py-4 px-6">{details.model}</td>
+                  </tr>
+                  <tr className="bg-gray-50 border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th
+                      scope="row"
+                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      Price
+                    </th>
+                    <td className="py-4 px-6">{details.price}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="flex md:mt-64">
+            <button
+              type="button"
+              className="btn bg-red-700"
+              onClick={() => dispatch(removeCar(details))}
+            >
+              Delete car
+            </button>
+            <button
+              type="button"
+              className="btn bg-green-700"
+              onClick={() => dispatch(removeCar(details))}
+            >
+              Reserve car
+            </button>
           </div>
         </div>
-        <div className="flex md:mt-64 justify-around">
-          <button
-            type="button"
-            className="btn bg-red-700"
-            onClick={() => dispatch(removeCar(details))}
-          >
-            Delete car
-          </button>
-          <button
-            type="button"
-            className="btn bg-green-700"
-            onClick={() => dispatch(removeCar(details))}
-          >
-            Reserve car
-          </button>
-        </div>
       </div>
-    </div>
   );
 };
 export default CarDetails;
