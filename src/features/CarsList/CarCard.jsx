1904e708694e121d61a9b1fdffc29e80137fd56e
HanningtoneM <hanningtonepompyy@gmail.com>
2022-10-31 10:44:19 +0300

    Add Car Card model  and price

--- src/features/CarsList/CarCard.jsx
+++ src/features/CarsList/CarCard.jsx
@@ -12,7 +12,17 @@ const Car = ({
       <div className={styles.imgCnt}>
         <img src={image} alt={brand} className={styles.carImg} />
       </div>
-      
+      <div className="model-detail d-flex flex-column justify-content-center align-items-center">
+        <h3 className="h4 mt-3">{brand}</h3>
+        <div className="d-flex flex-row justify-content-center align-items-center gap-2 mt-2">
+          <h5 className="h6 pt-1">{model}</h5>
+        </div>
+        <h5 className={styles.price}>
+          $
+          {' '}
+          {price}
+        </h5>
+      </div>
     </div>
   </NavLink>
 );
