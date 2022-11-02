f919f956c29cc36c5f230f4713bfe5e0b5ccc8fc
BHDHakim <hakim.bhd@gmail.com>
2022-11-02 22:41:18 +0100

    Use ENV variables for the API base url

--- src/modules/http.js
+++ src/modules/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/v1';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/v1';
 
 const http = axios.create({ baseURL: API_BASE_URL });
 
