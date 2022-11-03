0df7bd8dca905acda6f1c44d48ff7f4b6e1aa784
HanningtoneM <hanningtonepompyy@gmail.com>
2022-11-03 10:31:47 +0300

    Add Cardetails test

---
+++ src/__tests__/carDetails.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import renderWithProviders from '../utils/test-utils';
+import CarDetailsPage from '../pages/CarDetailsPage';
+
+describe('CarDetails component', () => {
+  const { asFragment } = renderWithProviders(<CarDetailsPage />);
+  const firstRender = asFragment();
+
+  it('should render correctly', () => {
+    expect(firstRender).toMatchSnapshot();
+  });
+});
