623675b25b46f3c0f6f715c2d1b6a20bd24ffd1e
HanningtoneM <hanningtonepompyy@gmail.com>
2022-11-03 10:19:45 +0300

    Add home test render first

---
+++ src/__tests__/home.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import renderWithProviders from '../utils/test-utils';
+import CarsList from '../features/CarsList/CarsList';
+
+describe('Homepage component', () => {
+  const { asFragment } = renderWithProviders(<CarsList />);
+  const firstRender = asFragment();
+
+  it('should render correctly', () => {
+    expect(firstRender).toMatchSnapshot();
+  });
+});
