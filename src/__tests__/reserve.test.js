11e18a6e91cbddb58f2d850461955d1d9cc6ff79
HanningtoneM <hanningtonepompyy@gmail.com>
2022-11-03 10:45:38 +0300

    Fix snapshot issue

--- src/__tests__/reserve.test.js
+++ src/__tests__/reserve.test.js
@@ -3,12 +3,14 @@ import '@testing-library/jest-dom';
 import renderWithProviders from '../utils/test-utils';
 import MyReservationsPage from '../pages/MyReservationsPage';
 
-describe('CarDetails component', () => {
+describe('Reserve component', () => {
   const { asFragment } = renderWithProviders(<MyReservationsPage />);
   const firstRender = asFragment();
 
   it('should render correctly', () => {
     expect(firstRender).toMatchSnapshot();
   });
-
+  it('should render correctly after state change', () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
