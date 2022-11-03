a2d0a669d1ffddf632107d229d09e836ab5d8410
HanningtoneM <hanningtonepompyy@gmail.com>
2022-11-03 10:26:21 +0300

    Add afterstate test

--- src/__tests__/home.test.js
+++ src/__tests__/home.test.js
@@ -10,4 +10,7 @@ describe('Homepage component', () => {
   it('should render correctly', () => {
     expect(firstRender).toMatchSnapshot();
   });
+  it('should render correctly after state change', () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
